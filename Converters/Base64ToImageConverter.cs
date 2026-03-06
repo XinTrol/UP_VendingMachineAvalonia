2aef098b34b64dc85206b8337c9222efc9882044
EgorSavukov <egor.sav0306@mail.ru>
2026-03-06 16:23:01 +0300

    Реализован конвертер для перевода потока байт, в изображение.

---
+++ Converters/Base64ToImageConverter.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using System;
+using System.Globalization;
+using System.IO;
+namespace UP_4.Converters
+{
+    public class Base64ToImageConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is string base64String && !string.IsNullOrEmpty(base64String))
+            {
+                try
+                {
+                    byte[] imageBytes = System.Convert.FromBase64String(base64String);
+                    var stream = new MemoryStream(imageBytes);
+
+                    return new Bitmap(stream);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
